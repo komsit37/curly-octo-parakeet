215942b3cf24c8c10b78943a67e963d553d105d7
gab <komsit37@gmail.com>
2015-07-29 00:00:21 +0900

    check response status code in get calendar

--- usage/calendar/calendar2.js
+++ usage/calendar/calendar2.js
@@ -3,10 +3,10 @@ var calendar = require('../../lib/calendar');
 var logger = require('../../lib/logger');
 
 //test rate limit
-calendar.getCalendar('3266217').then(logger.info);
-calendar.getCalendar('3266217', {month:1}).then(logger.info);
-calendar.getCalendar('3266217', {month:1}).then(logger.info);
-calendar.getCalendar('3266217', {month:1}).then(logger.info);
-calendar.getCalendar('3266217', {month:1}).then(logger.info);
-calendar.getCalendar('3266217', {month:1}).then(logger.info);
-calendar.getCalendar('3266217', {month:1}).then(logger.info);
+calendar.getCalendar('326621fefe7').then(logger.info).catch(logger.error);
+//calendar.getCalendar('3266217', {month:1}).then(logger.info);
+//calendar.getCalendar('3266217', {month:1}).then(logger.info);
+//calendar.getCalendar('3266217', {month:1}).then(logger.info);
+//calendar.getCalendar('3266217', {month:1}).then(logger.info);
+//calendar.getCalendar('3266217', {month:1}).then(logger.info);
+//calendar.getCalendar('3266217', {month:1}).then(logger.info);
